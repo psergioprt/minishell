04922bd044165db873c38c92305e8328b75b32c6
Whyflyy <cavadasribeiro.joao@gmail.com>
2025-02-13 00:55:01 +0000

    Redirects apparently working except cat << eof < Makefile, need norm

--- src/redirects_skip_functions.c
+++ src/redirects_skip_functions.c
@@ -6,14 +6,14 @@
 /*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 18:19:26 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/02/12 23:36:39 by jcavadas         ###   ########.fr       */
+/*   Updated: 2025/02/13 00:52:59 by jcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 void	skip_redirection_plus_target_cmd_support(t_node **current, \
-		t_node **tmp, t_node **prev, t_node **tokens)
+	t_node **tmp, t_node **prev, t_node **tokens)
 {
 	t_node	*target_node;
 
@@ -48,7 +48,6 @@ void skip_redirection_plus_target_cmd(t_cmd *cmd_list)
 		current = cmd->tokens;
 		while (current)
 		{
-			// Stop processing if the current token is a PIPE
 			if (current->type == PIPE)
 				break;
 
@@ -64,35 +63,6 @@ void skip_redirection_plus_target_cmd(t_cmd *cmd_list)
 	}
 }
 
-
-/* void	skip_redirection_plus_target_cmd(t_cmd *cmd_list)
-{
-	t_cmd	*cmd;
-	t_node	*prev;
-	t_node	*current;
-	t_node	*tmp;
-
-	cmd = cmd_list;
-	while (cmd)
-	{
-		prev = NULL;
-		current = cmd->tokens;
-		while (current)
-		{
-			if (current->type != NONE && current->type != HEREDOC && \
-					current->type != PIPE)
-				skip_redirection_plus_target_cmd_support(&current, \
-						&tmp, &prev, &cmd->tokens);
-			else
-			{
-				prev = current;
-				current = current->next;
-			}
-		}
-		cmd = cmd->next;
-	}
-}
- */
 void	skip_redirection_plus_target_support(t_node **current, t_node **tmp)
 {
 	t_node	*target_node;
@@ -139,35 +109,3 @@ void skip_redirection_plus_target(t_minishell *mini)
 	}
 	current = mini->tokenlst;
 }
-
-
-/* void	skip_redirection_plus_target(t_minishell *mini)
-{
-	t_node	*prev;
-	t_node	*current;
-	t_node	*tmp;
-
-	prev = NULL;
-	current = mini->tokenlst;
-	skip_redirection_plus_target_cmd(mini->commands);
-	while (current)
-	{
-		printf("current: %s\n", current->token);
-		if (current->type != NONE && current->type != HEREDOC && \
-				current->type != PIPE)
-		{
-			skip_redirection_plus_target_support(&current, &tmp);
-			if (prev)
-				prev->next = current;
-			else
-				mini->tokenlst = current;
-		}
-		else
-		{
-			prev = current;
-			current = current->next;
-		}
-	}
-	current = mini->tokenlst;
-}
- */
